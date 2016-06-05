9a6aebc759f8126a28be4fb44e90cdbc6d1c8ce8
wangli <leeowenowen@gmail.com>
2016-06-05 09:55:07 +0800

    add dependencies

---
+++ src/client/android/mwb/app/src/main/java/com/owo/mwb/App.java
@@ -0,0 +1,14 @@
+package com.owo.mwb;
+
+import android.app.Application;
+
+/**
+ * Created by wangli on 16-6-5.
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
